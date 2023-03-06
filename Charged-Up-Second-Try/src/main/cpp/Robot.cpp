9e875815777f8240ffaac81c81a50dfb2d90f397
ocartsonis <ocartsonis@gmail.com>
2023-03-06 16:11:24 -0700

    DEeZ pt2@
    well i hope is work

--- Charged-Up-Second-Try/src/main/cpp/Robot.cpp
+++ Charged-Up-Second-Try/src/main/cpp/Robot.cpp
@@ -3,6 +3,9 @@
 #include <frc/XboxController.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/motorcontrol/PWMTalonSRX.h>
+#include <frc/SerialPort.h>
+#include <stdio.h>
+#include <iostream>
 
 frc::PWMTalonSRX m_left{0};
 frc::PWMTalonSRX m_right{1};
@@ -10,6 +13,8 @@ frc::PWMTalonSRX m_left2{2};
 frc::PWMTalonSRX m_right2{3};
 frc::DifferentialDrive m_robotDriveFront{m_left, m_right};
 frc::DifferentialDrive m_robotDriveRear{m_left2, m_right2};
+
+frc::SerialPort arduinoSerial {9600, frc::SerialPort::Port::kMXP, 8, frc::SerialPort::Parity::kParity_None, frc::SerialPort::StopBits::kStopBits_One};
 
 frc::XboxController m_controller{0};
 frc::Timer m_timer;
@@ -28,11 +33,17 @@ class Robot : public frc::TimedRobot {
   }
 
   void AutonomousPeriodic() override {
+    
   }
 
   void TeleopInit() override {}
 
   void TeleopPeriodic() override {
+    char data[256];
+    int count = arduinoSerial.Read(data, sizeof(data));
+    int value = atoi(data);
+    std::cout << "My Variable: " << data << std::endl;
+
     double speed = m_controller.GetRawAxis(5);
     double turn = m_controller.GetRawAxis(2);
     m_robotDriveFront.ArcadeDrive(speed, turn);
