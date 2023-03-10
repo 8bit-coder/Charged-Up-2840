ae7e41a6b18ee33f0100c5aef2ff0489fe56abb5
8bit-coder <patrog7@gmail.com>
2023-03-09 17:28:04 -0700

    deezing time?
    deez nuts

--- Charged-Up-Second-Try/src/main/cpp/Robot.cpp
+++ Charged-Up-Second-Try/src/main/cpp/Robot.cpp
@@ -1,16 +1,25 @@
 #include <frc/TimedRobot.h>
+#include <frc/DigitalInput.h>
 #include <frc/Timer.h>
 #include <frc/XboxController.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <frc/motorcontrol/PWMTalonSRX.h>
 #include <frc/SerialPort.h>
+#include <ctre/Phoenix.h>
 #include <stdio.h>
 #include <iostream>
+
+ctre::phoenix::motorcontrol::can::TalonFX testMotor(0);
+
+auto sensorCollectionMotor1 = testMotor.GetSensorCollection();
+
+double position = sensorCollectionMotor1.GetIntegratedSensorAbsolutePosition();
 
 frc::PWMTalonSRX m_left{0};
 frc::PWMTalonSRX m_right{1};
 frc::PWMTalonSRX m_left2{2};
 frc::PWMTalonSRX m_right2{3};
+frc::PWMTalonSRX m_grabber{4};
 frc::DifferentialDrive m_robotDriveFront{m_left, m_right};
 frc::DifferentialDrive m_robotDriveRear{m_left2, m_right2};
 
@@ -21,12 +30,21 @@ frc::Timer m_timer;
 
 class Robot : public frc::TimedRobot {
  public:
+ frc::DigitalInput* lms_inward;
+ frc::DigitalInput* lms_outward;
+
   Robot() {
     m_right.SetInverted(true);
     m_right2.SetInverted(true);
     m_robotDriveFront.SetExpiration(100_ms);
     m_robotDriveRear.SetExpiration(100_ms);
     m_timer.Start();
+  }
+
+  void RobotInit() override {
+    // Instantiate the DIO objects for port 0 and port 1
+    lms_inward = new frc::DigitalInput(0);
+    lms_outward = new frc::DigitalInput(1);
   }
 
   void AutonomousInit() override {
@@ -36,18 +54,24 @@ class Robot : public frc::TimedRobot {
     
   }
 
-  void TeleopInit() override {}
+  void TeleopInit() override {
+  }
 
   void TeleopPeriodic() override {
-    char data[256];
-    int count = arduinoSerial.Read(data, sizeof(data));
-    int value = atoi(data);
-    std::cout << "My Variable: " << data << std::endl;
-
     double speed = m_controller.GetRawAxis(5);
     double turn = m_controller.GetRawAxis(2);
     m_robotDriveFront.ArcadeDrive(speed, turn);
     m_robotDriveRear.ArcadeDrive(speed, turn);
+
+    bool lms_inward_state = lms_inward->Get();
+    bool lms_outward_state = lms_outward->Get();
+    if(m_controller.GetRawButton(5) && lms_inward_state == false){
+      m_grabber.Set(0.2);
+    }else if(m_controller.GetRawButton(6) && lms_outward_state == false){
+      m_grabber.Set(-0.2);
+    }else{
+      m_grabber.Set(0);
+    }
   }
 
   void TestInit() override {}
